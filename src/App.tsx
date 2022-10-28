854ec2e74b363cb94ce895e708c35d112ece3d06
miketabb33 <miketabb33@gmail.com>
2022-10-28 15:29:36 -0700

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div className="App">hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
